7b90ef49d4eaa33107f5faec986894c5c6dbe3d0
Mohammad_Al_Hallaq <mohammad_al_hallaq@unifisolutions.io>
2023-12-30 06:42:43 -0800

    works as theme in unocss

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,33 +1,55 @@
-import { FC } from 'react'
-interface Layout {
-}
+import { FC } from "react";
+interface Layout {}
 
-const Layout : FC<Layout> = ({})=> {
-    return (
-        <>
-        <div className='plg bg-[#eaeaea] '>
-            <button onClick={()=> {
-                document.body.classList.toggle('dark');
-
-            }}>button</button>
-            <h4>colors</h4>
-            <ul>
-                <li>
-                    primary : <button className='bg-red dark:bg-yellow p-8px'></button>
-                </li>
-                <li>
-                    secondary : <button className='bg-teal p-8px'></button>
-                </li>
-                <li>
-                    text color  : <button className='bg-black p-8px' ></button>
-                </li>
-                <li>
-                    backgrounds : <button className='bg-gray p-8px'></button>
-                </li>
-
-            </ul>
-        </div>
-        </>
-    )
-}
-export default Layout
+const Layout: FC<Layout> = ({}) => {
+  return (
+    <>
+      <div className="plg bg-container text-text ">
+        <button
+          onClick={() => {
+            document.body.classList.remove("sky");
+            document.body.classList.remove("light");
+            document.body.classList.add("dark");
+          }}
+        >
+          dark
+        </button>
+        <button
+          onClick={() => {
+            document.body.classList.remove("dark");
+            document.body.classList.remove("sky");
+            document.body.classList.add("light");
+          }}
+        >
+          light
+        </button>
+        <button
+          onClick={() => {
+            document.body.classList.remove("dark");
+            document.body.classList.remove("light");
+            document.body.classList.add("sky");
+          }}
+        >
+          dark
+        </button>
+        <h4 >colors</h4>
+        <ul className="border-1px border-solid border-border plg rounded-lg">
+          <li>
+            primary : <button className="bg-primary  p-8px"></button>
+          </li>
+          
+          <li>
+            secondary : <button className="bg-primary-border p-8px"></button>
+          </li>
+          <li>
+            text color : <button className="bg-text p-8px"></button>
+          </li>
+          <li>
+            backgrounds : <button className="bg-container p-8px"></button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+export default Layout;
