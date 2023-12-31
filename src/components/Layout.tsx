1d5c0d5fd198f058ae57fa1816a1c5ac911a97fc
Mohammad_Al_Hallaq <mohammad_al_hallaq@unifisolutions.io>
2023-12-30 23:21:46 -0800

    init some files

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
+import DineInIcon from "./icons/DineInIcon";
 interface Layout {}
 
 const Layout: FC<Layout> = ({}) => {
+  const name = async () => {};
   return (
     <>
       <div className="plg bg-container text-text ">
@@ -23,31 +25,35 @@ const Layout: FC<Layout> = ({}) => {
         >
           light
         </button>
-        <button
-          onClick={() => {
-            document.body.classList.remove("dark");
-            document.body.classList.remove("light");
-            document.body.classList.add("sky");
-          }}
-        >
-          dark
-        </button>
-        <h4 >colors</h4>
-        <ul className="border-1px border-solid border-border plg rounded-lg">
-          <li>
-            primary : <button className="bg-primary  p-8px"></button>
-          </li>
-          
-          <li>
-            secondary : <button className="bg-primary-border p-8px"></button>
-          </li>
-          <li>
-            text color : <button className="bg-text p-8px"></button>
-          </li>
-          <li>
-            backgrounds : <button className="bg-container p-8px"></button>
-          </li>
-        </ul>
+
+        <h4>colors</h4>
+        <div className="plg mlg border-1px border-solid border-gray rounded-xl">
+          <div className="bg-base-container p-lg rounded-xl">
+            <div className="flex ">
+              <div className="me-sm">
+                <button className="text-base-blue border-none w-40px h-40px rounded-full bg-base-light-blue">
+                  9
+                </button>
+              </div>
+              <div className="me-sm">
+                <button className="text-base-orange border-none w-40px h-40px rounded-full bg-base-light-orange">
+                  5
+                </button>
+              </div>
+              <div className="me-sm">
+                <div className="w-40px text-red">
+                  <div className="i-logos-vue text-3xl" />
+
+                  <div className="w-full flex items-center justify-center gap-x-4 text-4xl p-2 mt-4">
+                    <div className="i-vscode-icons:file-type-light-pnpm" />
+                    <div className="i-vscode-icons:file-type-light-pnpm?mask text-red-300" />
+                  </div>
+                  <DineInIcon></DineInIcon>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/icons/index.ts
@@ -0,0 +1,5 @@
+import dineInIcon from "./dine-in.svg";
+
+export const customIcons = {
+  "dine-in": dineInIcon,
+};
