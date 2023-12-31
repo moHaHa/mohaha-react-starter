c94c6701cc0b9f69ddbf07e3e2f6bcd42bde5db1
Mohammad_Al_Hallaq <mohammad_al_hallaq@unifisolutions.io>
2023-12-31 03:25:47 -0800

    vscode config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import React from "@vitejs/plugin-react";
-import Unocss from "unocss/vite";
-import { defineConfig } from "vite";
-import checker from "vite-plugin-checker";
+import React from '@vitejs/plugin-react';
+import Unocss from 'unocss/vite';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Unocss(), React(), checker({ typescript: true })],
+	plugins: [Unocss(), React(), checker({ typescript: true })],
 });
